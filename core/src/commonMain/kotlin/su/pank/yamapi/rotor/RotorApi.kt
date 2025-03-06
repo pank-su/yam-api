0278c75ae4ad3d3d4c3f06fefc31255d49782819
pank-su <pank@pank.su>
2025-03-06 14:34:10 +0300

    :fire: удаление api для работы c радио

--- core/src/commonMain/kotlin/su/pank/yamapi/rotor/RotorApi.kt
+++ core/src/commonMain/kotlin/su/pank/yamapi/rotor/RotorApi.kt
@@ -5,22 +5,5 @@ import su.pank.yamapi.exceptions.ExperimentalYaMusicApi
 
 @ExperimentalYaMusicApi
 class RotorApi(private val client: YamApiClient) {
-//    suspend fun accountStatus() = client.request<Status>("rotor", "account", "status")
-//
-//    suspend fun stationsDashboard() = client.request<Dashboard>("rotor", "stations", "dashboard")
-//
-//    suspend fun stations(language: Language = Language.ru) = client.request<List<StationResult>>(
-//        listOf("rotor", "stations", "list"),
-//        body = hashMapOf("language" to Json.encodeToString(language).removeCarets())
-//    )
-//
-//    // TODO: работа с сессиями
-//    suspend fun sessionNew(seeds: List<String>) =
-//        client.requestPost<Session>(
-//            "rotor",
-//            "session",
-//            "new",
-//            body = hashMapOf("seeds" to "[" + seeds.joinToString(",") { "\"$it\"" } + "]",
-//                "includeTracksInResponse" to "true")
-//        )
+
 }
