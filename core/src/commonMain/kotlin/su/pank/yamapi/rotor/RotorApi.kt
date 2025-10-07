441a9aadbd728476d3b2f9eb98619627beb1176e
pank-su <pank@pank.su>
2025-10-07 16:38:33 +0300

    :fire: удаление лишних запросов по плейлистам вне PlaylistsApi :recycle: анотация Experimental теперь придерживается общего нейминга

--- core/src/commonMain/kotlin/su/pank/yamapi/rotor/RotorApi.kt
+++ core/src/commonMain/kotlin/su/pank/yamapi/rotor/RotorApi.kt
@@ -1,14 +1,14 @@
 package su.pank.yamapi.rotor
 
 import su.pank.yamapi.YamApiClient
-import su.pank.yamapi.exceptions.ExperimentalYaMusicApi
+import su.pank.yamapi.exceptions.ExperimentalYamApi
 
 /**
  * API для работы с ротором (радио).
  *
  * @param client Клиент YamApiClient.
  */
-@ExperimentalYaMusicApi
+@ExperimentalYamApi
 class RotorApi(
     private val client: YamApiClient,
 )
