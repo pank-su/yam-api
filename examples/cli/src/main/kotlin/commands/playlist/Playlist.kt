100440ad6d460a630d36d3b27e788d5f681828a1
pank-su <pank@pank.su>
2025-10-15 21:18:39 +0300

    :fire: удаление бесполезного кода

--- examples/cli/src/main/kotlin/commands/playlist/Playlist.kt
+++ examples/cli/src/main/kotlin/commands/playlist/Playlist.kt
@@ -1,17 +1,10 @@
 package commands.playlist
 
 import com.github.ajalt.clikt.core.CliktCommand
-import data.yamApiClient
 import kotlinx.coroutines.runBlocking
 
 class Playlist: CliktCommand() {
     override fun run() = runBlocking{
-        val yamApiClient = yamApiClient()
-        val playlist = yamApiClient.albumsWithTracks(4193502)
-        val shortTracks = playlist.volumes?.first()
-        val tracks = yamApiClient.tracks(*(shortTracks!!.map { it.id }.toTypedArray()))
-        tracks.forEach {
-            echo(it)
-        }
+
     }
 }
